ea31e7a23a9f100ea7cc4406e54d1b8a23dec4ac
erikras12 <erikras12@hotmail.com>
2025-03-03 15:08:40 +0000

    add 80

--- energy-mvc/controllers/stats.js
+++ energy-mvc/controllers/stats.js
@@ -1,8 +1,10 @@
 const { Stats } =  require("../models/Stats")
+const { User } =  require("../models/User")
 const axios = require('axios')
 
 async function getStats(req, res) {
     try {
+      console.log("getstats hit");
         const userId = req.body.registration_id;
 
         const meterData = await Stats.getMeterDataForUserById(userId)
@@ -19,6 +21,30 @@ async function getStats(req, res) {
     }
   }
 
+async function getDonut(req, res) {
+    try {
+        const userId = req.body.registration_id;
+
+        const donutData = await User.getOneById(userId)
+        
+
+        const data = { data: { donut: donutData } };
+        // const postcode = donutData.data.donut.postcode;
+
+        // const response = await axios.post('http://energy-python:3001/<donut-endpoint>', data)
+        const response_api = await axios.get(`https://api.carbonintensity.org.uk/regional/postcode/AL7`)
+
+        const carbonIntensityData = response_api.data
+        const generationMix = carbonIntensityData.data[0].data[0].generationmix
+
+        // res.status(200).json({ "html": response.data.visualisation_html })
+        res.status(200).json(generationMix)
+    } catch (err) {
+      res.status(404).json({ error: err.message });
+    }
+  }
+
 module.exports = {
     getStats,
+    getDonut
 }
