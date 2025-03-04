39f0389bf4b3700e732f2fdb7676b3121b7c60c7
allredmatt <allredmatt@gmail.com>
2025-03-04 14:01:40 +0000

    Add live octopus data

--- energy-mvc/controllers/stats.js
+++ energy-mvc/controllers/stats.js
@@ -1,12 +1,29 @@
-const { Stats } =  require("../models/Stats")
+const { User } =  require("../models/User")
+const {fetchExternalMeterData, fetchExternalCO2Data} = require("../services/externalAPIService")
 const axios = require('axios')
 
 async function getStats(req, res) {
     try {
         const userId = req.body.registration_id;
 
-        const meterData = await Stats.getMeterDataForUserById(userId)
-        const intensityData = await Stats.getIntensityDataForUserById(userId)
+        const currentUser = await User.getOneById(userId)
+
+        const [octopusMeterData, energyCarbonData] = await Promise.all([
+                  fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial),
+                  fetchExternalCO2Data(currentUser.postcode)
+                ])
+        // const octopusMeterData = await fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial)
+        // const energyCarbonData = await fetchExternalCO2Data(currentUser.postcode)
+
+        console.log(energyCarbonData.data.data.data);
+
+        const meterData = octopusMeterData.data.results.map(reading => {
+          return {datapoint: reading.consumption, start_datetime: reading.interval_start, end_datetime: reading.interval_end}
+        })
+
+        const intensityData = energyCarbonData.data.data.data.map(carbon => {
+          return {datapoint: carbon.intensity.forecast, start_datetime: carbon.from, end_datetime: carbon.to}
+        })
 
         const data = {data: {meter: meterData, intensity: intensityData}}
 
@@ -14,6 +31,25 @@ async function getStats(req, res) {
 
         res.status(200).json({ "html": response.data.visualisation_html })
         //res.status(200).json({data: {meter: meterData, intensity: intensityData}})
+    } catch (err) {
+      res.status(404).json({ error: err.message });
+    }
+  }
+
+async function getDonut(req, res) {
+    try {
+        const userId = req.body.registration_id;
+
+        const userData = await User.getOneById(userId)
+
+        const response_api = await axios.get(`https://api.carbonintensity.org.uk/regional/postcode/${userData.postcode}`)
+
+        const carbonIntensityData = response_api.data
+        const generationMix = carbonIntensityData.data[0].data[0].generationmix
+
+        const response = await axios.post('http://energy-python:3001/generate-pie-visualisation', generationMix)
+
+        res.status(200).json({ "html": response.data.visualisation_html })
     } catch (err) {
       res.status(404).json({ error: err.message });
     }
--- energy-mvc/models/User.js
+++ energy-mvc/models/User.js
@@ -1,7 +1,8 @@
 const db = require("../db/connect")
+const axios = require('axios')
 
 class User {
-    constructor({ registration_id, name, password, username, email, postcode, region}) {
+    constructor({ registration_id, name, password, username, email, postcode, region, meter_mpan=null, meter_serial=null, api_key =null}) {
         this.registration_id = registration_id;
         this.username = username;
         this.password = password;
@@ -9,6 +10,9 @@ class User {
         this.postcode = postcode;
         this.email = email;
         this.region = region;
+        this.api_key = api_key
+        this.meter_mpan = meter_mpan
+        this.meter_serial = meter_serial
     }
 
     static async getOneByUsername(username) {
@@ -28,15 +32,33 @@ class User {
     }
 
     static async create(data) {
-        const { name, password, username, email, postcode, region } = data;
+        const { name, password, username, email, postcode, region, api_key } = data;
         if(username == undefined || password == undefined) throw Error("Ensure username and password are both provided")
-        let response = await db.query("INSERT INTO registration_info (name, password, username, email, postcode, region) VALUES ($1, $2, $3, $4, $5, $6) RETURNING registration_id;",
-            [name, password, username, email, postcode, region]);
-        const newId = response.rows[0].registration_id_id;
+        let response = await db.query("INSERT INTO registration_info (name, password, username, email, postcode, region, api_key) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING registration_id;",
+            [name, password, username, email, postcode, region, api_key]);
+        const newId = response.rows[0].registration_id;
         const newUser = await User.getOneById(newId);
+        newUser.password = "#"
+        newUser.updateMeterInfo(data.account_number, data.api_key)
         return newUser;
     }
 
+    async updateMeterInfo(account, api) {
+        try{
+            const token = Buffer.from(`${api}:`).toString('base64');
+            const response = await axios.get(`https://api.octopus.energy/v1/accounts/${account}`, {
+                headers: {
+                    "Authorization": `Basic ${token}`
+                }
+            })
+            const data = response.data
+            this.meter_mpan = data.properties[0].electricity_meter_points[0].mpan
+            this.meter_serial = data.properties[0].electricity_meter_points[0].meters[0].serial_number
+            db.query("UPDATE registration_info SET meter_mpan = $1, meter_serial = $2 WHERE registration_id = $3;", [this.meter_mpan, this.meter_serial, this.registration_id])
+        } catch(err) {
+            console.error("Error: ", err)
+        }
+    }
 }
 
 module.exports = { User }
