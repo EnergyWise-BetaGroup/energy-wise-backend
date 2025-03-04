8c49a86b5c37e4586d955a407bd93b9be161a703
allredmatt <allredmatt@gmail.com>
2025-03-04 21:11:49 +0000

    Add cache for fetched data

--- energy-mvc/controllers/stats.js
+++ energy-mvc/controllers/stats.js
@@ -1,92 +1,158 @@
-const { User } =  require("../models/User")
-const {fetchExternalMeterData, fetchExternalCO2Data} = require("../services/externalAPIService")
-const axios = require('axios')
+const { User } = require("../models/User");
+const {
+    fetchExternalMeterData,
+    fetchExternalCO2Data,
+} = require("../services/externalAPIService");
+const axios = require("axios");
+
+const cache = {};
 
 async function getStats(req, res) {
     try {
         const userId = req.body.registration_id;
+        const currentTime = new Date();
+        const timeIn30Mins = new Date();
+        timeIn30Mins.setMinutes(timeIn30Mins.getMinutes() + 30);
 
-        const currentUser = await User.getOneById(userId)
+        if (cache.hasOwnProperty(userId) && currentTime < cache.userId.date) {
+            const data = cache.userId.data;
+        } else {
+            const currentUser = await User.getOneById(userId);
 
-        const [octopusMeterData, energyCarbonData] = await Promise.all([
-                  fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial),
-                  fetchExternalCO2Data(currentUser.postcode)
-                ])
-        // const octopusMeterData = await fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial)
-        // const energyCarbonData = await fetchExternalCO2Data(currentUser.postcode)
+            const [octopusMeterData, energyCarbonData] = await Promise.all([
+                fetchExternalMeterData(
+                    currentUser.api_key,
+                    currentUser.meter_mpan,
+                    currentUser.meter_serial
+                ),
+                fetchExternalCO2Data(currentUser.postcode),
+            ]);
+            // const octopusMeterData = await fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial)
+            // const energyCarbonData = await fetchExternalCO2Data(currentUser.postcode)
 
-        const meterData = octopusMeterData.data.results.map(reading => {
-          return {datapoint: reading.consumption, start_datetime: reading.interval_start, end_datetime: reading.interval_end}
-        })
+            const meterData = octopusMeterData.data.results.map((reading) => {
+                return {
+                    datapoint: reading.consumption,
+                    start_datetime: reading.interval_start,
+                    end_datetime: reading.interval_end,
+                };
+            });
 
-        const intensityData = energyCarbonData.data.data.data.map(carbon => {
-          return {datapoint: carbon.intensity.forecast, start_datetime: carbon.from, end_datetime: carbon.to}
-        })
+            const intensityData = energyCarbonData.data.data.data.map(
+                (carbon) => {
+                    return {
+                        datapoint: carbon.intensity.forecast,
+                        start_datetime: carbon.from,
+                        end_datetime: carbon.to,
+                    };
+                }
+            );
 
-        const data = {data: {meter: meterData, intensity: intensityData}}
+            const data = {
+                data: { meter: meterData, intensity: intensityData },
+            };
+            cache[userId] = { date: timeIn30Mins, data: data };
+        }
 
-        const response = await axios.post('http://energy-python:3001/generate-visualisation', data)
+        const response = await axios.post(
+            "http://energy-python:3001/generate-visualisation",
+            data
+        );
 
-        res.status(200).json({ "html": response.data.visualisation_html })
+        res.status(200).json({ html: response.data.visualisation_html });
         //res.status(200).json({data: {meter: meterData, intensity: intensityData}})
     } catch (err) {
-      res.status(404).json({ error: err.message });
+        res.status(404).json({ error: err.message });
     }
-  }
+}
 
 async function getDonut(req, res) {
     try {
         const userId = req.body.registration_id;
+        
+        const userData = await User.getOneById(userId);
 
-        const userData = await User.getOneById(userId)
+        const response_api = await axios.get(
+            `https://api.carbonintensity.org.uk/regional/postcode/${userData.postcode}`
+        );
 
-        const response_api = await axios.get(`https://api.carbonintensity.org.uk/regional/postcode/${userData.postcode}`)
+        const carbonIntensityData = response_api.data;
+        const generationMix = carbonIntensityData.data[0].data[0].generationmix;
 
-        const carbonIntensityData = response_api.data
-        const generationMix = carbonIntensityData.data[0].data[0].generationmix
+        const response = await axios.post(
+            "http://energy-python:3001/generate-pie-visualisation",
+            generationMix
+        );
 
-        const response = await axios.post('http://energy-python:3001/generate-pie-visualisation', generationMix)
-
-        res.status(200).json({ "html": response.data.visualisation_html })
+        res.status(200).json({ html: response.data.visualisation_html });
     } catch (err) {
-      res.status(404).json({ error: err.message });
+        res.status(404).json({ error: err.message });
     }
-  }
+}
 
-  async function getGauge(req, res) {
+async function getGauge(req, res) {
     try {
         const userId = req.body.registration_id;
+        const currentTime = new Date();
+        const timeIn30Mins = new Date();
+        timeIn30Mins.setMinutes(timeIn30Mins.getMinutes() + 30);
 
-        const currentUser = await User.getOneById(userId)
+        if (cache.hasOwnProperty(userId) && currentTime < cache.userId.date) {
+            const data = cache.userId.data;
+        } else {
+            const userId = req.body.registration_id;
 
-        const [octopusMeterData, energyCarbonData] = await Promise.all([
-                  fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial),
-                  fetchExternalCO2Data(currentUser.postcode)
-                ])
-        // const octopusMeterData = await fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial)
-        // const energyCarbonData = await fetchExternalCO2Data(currentUser.postcode)
+            const currentUser = await User.getOneById(userId);
 
-        const meterData = octopusMeterData.data.results.map(reading => {
-          return {datapoint: reading.consumption, start_datetime: reading.interval_start, end_datetime: reading.interval_end}
-        })
+            const [octopusMeterData, energyCarbonData] = await Promise.all([
+                fetchExternalMeterData(
+                    currentUser.api_key,
+                    currentUser.meter_mpan,
+                    currentUser.meter_serial
+                ),
+                fetchExternalCO2Data(currentUser.postcode),
+            ]);
+            // const octopusMeterData = await fetchExternalMeterData(currentUser.api_key, currentUser.meter_mpan, currentUser.meter_serial)
+            // const energyCarbonData = await fetchExternalCO2Data(currentUser.postcode)
 
-        const intensityData = energyCarbonData.data.data.data.map(carbon => {
-          return {datapoint: carbon.intensity.forecast, start_datetime: carbon.from, end_datetime: carbon.to}
-        })
+            const meterData = octopusMeterData.data.results.map((reading) => {
+                return {
+                    datapoint: reading.consumption,
+                    start_datetime: reading.interval_start,
+                    end_datetime: reading.interval_end,
+                };
+            });
 
-        const data = {data: {meter: meterData, intensity: intensityData}}
+            const intensityData = energyCarbonData.data.data.data.map(
+                (carbon) => {
+                    return {
+                        datapoint: carbon.intensity.forecast,
+                        start_datetime: carbon.from,
+                        end_datetime: carbon.to,
+                    };
+                }
+            );
 
-        const response = await axios.post('http://energy-python:3001/generate-gauge-visualisation', data)
+            const data = {
+                data: { meter: meterData, intensity: intensityData },
+            };
+            cache[userId] = { date: timeIn30Mins, data: data };
+        }
 
-        res.status(200).json({ "html": response.data.visualisation_html })
+        const response = await axios.post(
+            "http://energy-python:3001/generate-gauge-visualisation",
+            data
+        );
+
+        res.status(200).json({ html: response.data.visualisation_html });
         //res.status(200).json({data: {meter: meterData, intensity: intensityData}})
     } catch (err) {
-      res.status(404).json({ error: err.message });
+        res.status(404).json({ error: err.message });
     }
-  }
+}
 
 module.exports = {
     getStats,
     getDonut,
-    getGauge
-}
+    getGauge,
+};
